f936c7e3d88563a011fa63384fb9c8073fa28665
Sophia Dong <sophia@Sophias-MBP.home>
2018-09-29 11:50:14 -0400

    added heuristic_test

--- test/implementation.py
+++ test/implementation.py
@@ -3,25 +3,15 @@
 #
 # Feel free to use this code in your own projects, including commercial projects
 # License: Apache v2.0 <http://www.apache.org/licenses/LICENSE-2.0.html>
-
-class SimpleGraph:
-    def __init__(self):
-        self.edges = {}
-
-    def neighbors(self, id):
-        return self.edges[id]
-
-
-example_graph = SimpleGraph()
-example_graph.edges = {
-    'A': ['B'],
-    'B': ['A', 'C', 'D'],
-    'C': ['A'],
-    'D': ['E', 'A'],
-    'E': ['B']
-}
-
 import collections
+import math
+
+# class SimpleGraph:
+#     def __init__(self):
+#         self.edges = {}
+#
+#     def neighbors(self, id):
+#         return self.edges[id]
 
 
 class Queue:
@@ -68,6 +58,7 @@ def draw_grid(graph, width=2, **style):
 
 
 # data from main article
+# used to test
 DIAGRAM1_WALLS = [from_id_width(id, width=30) for id in
                   [21, 22, 51, 52, 81, 82, 93, 94, 111, 112, 123, 124, 133, 134, 141, 142, 153, 154, 163, 164, 171, 172,
                    173, 174, 175, 183, 184, 193, 194, 201, 202, 203, 204, 205, 213, 214, 223, 224, 243, 244, 253, 254,
@@ -131,32 +122,6 @@ class PriorityQueue:
     def get(self):
         return heapq.heappop(self.elements)[1]
 
-
-def dijkstra_search(graph, start, goal):
-    frontier = PriorityQueue()
-    frontier.put(start, 0)
-    came_from = {}
-    cost_so_far = {}
-    came_from[start] = None
-    cost_so_far[start] = 0
-
-    while not frontier.empty():
-        current = frontier.get()
-
-        if current == goal:
-            break
-
-        for next in graph.neighbors(current):
-            new_cost = cost_so_far[current] + graph.cost(current, next)
-            if next not in cost_so_far or new_cost < cost_so_far[next]:
-                cost_so_far[next] = new_cost
-                priority = new_cost
-                frontier.put(next, priority)
-                came_from[next] = current
-
-    return came_from, cost_so_far
-
-
 # thanks to @m1sp <Jaiden Mispy> for this simpler version of
 # reconstruct_path that doesn't have duplicate entries
 
@@ -172,9 +137,16 @@ def reconstruct_path(came_from, start, goal):
 
 
 def heuristic(a, b):
+    # a = goal, b = other node
     (x1, y1) = a
     (x2, y2) = b
     return abs(x1 - x2) + abs(y1 - y2)
+
+def heuristic_test(a, b):
+    # a = goal, b = other node
+    (goal_x, goal_y) = a
+    (s_x, s_y) = b
+    return math.sqrt(2) * min(abs(s_x - goal_x), abs(s_y - goal_y)) + max(abs(s_x - goal_x), abs(s_y - goal_y)) - min(abs(s_x - goal_x), abs(s_y - goal_y))
 
 
 def a_star_search(graph, start, goal):
