8f2a23820a0a60e3bf8f47828c9af3532f161469
Sophia Dong <sophia@nbp-67-116.nbp.ruw.rutgers.edu>
2018-10-01 21:10:42 -0400

    added jordan's code parts

--- test/visualize_grid.py
+++ test/visualize_grid.py
@@ -1,24 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def visualize_grid(matrix):
+    # width and height of grid
+    w = 10
+    h = 10
 
-# width and height of grid
-w = 10
-h = 10
+    # generates 2d matrix size[w][h]
+    gridplot = [[0 for x in range(w)] for y in range(h)]
 
-# generates 2d matrix size[w][h]
-gridplot = [[0 for x in range(w)] for y in range(h)]
+    gridplot[0][0] = 1
+    gridplot[1][1] = 1
+    gridplot[2][2] = 1
+    gridplot[3][3] = 1
+    gridplot[4][4] = 1
+    gridplot[5][5] = 1
+    gridplot[6][6] = 1
+    gridplot[7][7] = 1
+    gridplot[8][8] = 1
+    gridplot[9][9] = 1
 
-gridplot[0][0] = 1
-gridplot[1][1] = 1
-gridplot[2][2] = 1
-gridplot[3][3] = 1
-gridplot[4][4] = 1
-gridplot[5][5] = 1
-gridplot[6][6] = 1
-gridplot[7][7] = 1
-gridplot[8][8] = 1
-gridplot[9][9] = 1
+    # should be 0 for non-obstacle, 1 for obstacle
 
-plt.matshow(gridplot)
-plt.show()
+    plt.matshow(matrix)
+    plt.show()
